1d7f9207007a5b12a648152ab01e214bb17b072a
Swhitzer <Swhitzer@yandex.ru>
2023-10-06 17:49:20 +0400

    yes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.createConnection(
-    "mongodb://localhost:27017/dbName",
-    {
-      "auth": {
-        "authSource": "admin"
-      },
-      "user": "admin",
-      "pass": "password"
-    }
-);
-
 mongoose.connect( process.env.MONGODB_URL , {
     useNewUrlParser: true,
     useCreateIndex: true,
