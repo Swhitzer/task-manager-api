139311016cb8a020a5f9e6024a404c6ce03c47e8
Swhitzer <Swhitzer@yandex.ru>
2023-10-06 18:07:16 +0400

    asd

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,6 @@ mongoose.connect( process.env.MONGODB_URL , {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    pass: process.env.MONGO_INITDB_ROOT_PASSWORD
+    user: "Swhitzer",
+    pass: "k7MPjp6D"
 });
