bbf7600f5b855ec4d8175da452307b9c2169cc72
Swhitzer <Swhitzer@yandex.ru>
2023-10-06 17:47:55 +0400

    Added user and pass idk

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect( process.env.MONGODB_URL, {
+mongoose.createConnection(
+    "mongodb://localhost:27017/dbName",
+    {
+      "auth": {
+        "authSource": "admin"
+      },
+      "user": "admin",
+      "pass": "password"
+    }
+);
+
+mongoose.connect( process.env.MONGODB_URL , {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pass: process.env.MONGO_INITDB_ROOT_PASSWORD
 });
