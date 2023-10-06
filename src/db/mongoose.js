8308729e33f4e7a08852369764e5ad90e302a754
Swhitzer <Swhitzer@yandex.ru>
2023-10-06 18:17:13 +0400

    asdf

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
+const db = require('mongodb');
+db.CreateUser()
 
 mongoose.connect( process.env.MONGODB_URL , {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    user: "Swhitzer",
-    pass: "k7MPjp6D"
+    useFindAndModify: false
 });
